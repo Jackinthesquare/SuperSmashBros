0f2b613482277307c9e5f4b26f19cd7b57258d6c
Jacky Liang <asheriffy@gmail.com>
2022-04-06 11:30:49 -0400

    design fighter screen

--- src/components/FighterScreen.js
+++ src/components/FighterScreen.js
@@ -3,10 +3,10 @@ const FighterScreen = ({isVisible, setVisible, selectedCharacter}) => {
         < div onClick={() => {setVisible(false)}} className="fighter-screen" style={{display: isVisible ? "block" : "none"}} >
             <span className="name-bg">{selectedCharacter.name}</span>
             <h2 className="name-fg">{selectedCharacter.name}</h2>
-            <img className="char-portrait" width="100" height="100" src={`https://www.smashbros.com/assets_v2/img/fighter/${selectedCharacter.name.toLowerCase()}/main.png`} />
-            <p className="fighter-desc">
+            <img className="char-portrait" src={`https://www.smashbros.com/assets_v2/img/fighter/${selectedCharacter.name.toLowerCase()}/main.png`} />
+            {/* <p className="fighter-desc">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt in laboriosam quo atque, error dolorem vel ex labore quasi corrupti sed magnam eos praesentium quidem autem. Necessitatibus, dolor. Impedit, laborum?
-            </p>
+            </p> */}
         </div >
     )
 }
